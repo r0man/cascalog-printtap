dccdc99be8b18149de05182880109bc6202379ff
Roman Scherer <roman.scherer@nugg.ad>
2013-02-20 12:41:36 +0100

    Changed project name.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject printtap "0.1.0-SNAPSHOT"
+(defproject cascalog-printtap "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
