0b54df0d216742c389dbb36282aa01c48d5036d6
Roman Scherer <roman.scherer@nugg.ad>
2013-04-03 11:52:53 +0200

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cascalog-printtap "0.1.1"
+(defproject cascalog-printtap "0.1.2-SNAPSHOT"
   :description "A Cascalog tap that prints tuples via Clojure's pr-str function."
   :url "https://github.com/r0man/cascalog-printtap"
   :license {:name "Eclipse Public License"
@@ -6,8 +6,8 @@
   :repositories {"conjars" "http://conjars.org/repo/"
                  "cloudera" "https://repository.cloudera.com/artifactory/cloudera-repos"}
   :profiles {:dev {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-cdh3u5"]]}}
-  :dependencies [[cascalog "1.10.0"]
-                 [org.clojure/clojure "1.4.0"]]
+  :dependencies [[cascalog "1.10.1"]
+                 [org.clojure/clojure "1.5.1"]]
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
   :java-source-paths ["src/jvm"]
